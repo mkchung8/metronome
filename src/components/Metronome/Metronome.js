bbb4bf7b946e472c059282b318501dbba54fdd28
Miyoung Chung <mkchung.8@gmail.com>
2020-11-30 23:15:34 -0700

    added buttons for sounds and modified layout

--- src/components/Metronome/Metronome.js
+++ src/components/Metronome/Metronome.js
@@ -5,12 +5,15 @@ import Card from "@material-ui/core/Card";
 import { makeStyles } from "@material-ui/core/styles";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import { IconButton } from "@material-ui/core";
+import { Container, Button, IconButton } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles({
   root: {
     padding: 50,
+    textAlign: "center"
+  }, 
+  sounds: {
     textAlign: "center"
   }
 });
@@ -18,26 +21,32 @@ const useStyles = makeStyles({
 function Metronome() {
   const classes = useStyles();
   return (
-    <Grid container className={classes.root}>
-      <Grid item xs={4}>
-        <IconButton>
-          <KeyboardArrowUpIcon fontSize="large" />
-        </IconButton>
-      </Grid>
-      <Grid item xs={4}>
-        <Card>
-          <Typography variant="h3">
-            138 BPM
+    <Container>
+      <Grid container className={classes.root}>
+        <Grid item xs={4}>
+          <IconButton>
+            <KeyboardArrowUpIcon fontSize="large" />
+          </IconButton>
+        </Grid>
+        <Grid item xs={4}>
+          <Card>
+            <Typography variant="h3">
+              138 BPM
         </Typography>
-        </Card>
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <IconButton>
+            <KeyboardArrowDownIcon fontSize="large" />
+          </IconButton>
+        </Grid>
       </Grid>
-      <Grid item xs={4}>
-        <IconButton>
-          <KeyboardArrowDownIcon fontSize="large" />
-        </IconButton>
-      </Grid>
-    </Grid>
-
+      <Container className={classes.sounds}>
+        <Button>Beep</Button>
+        <Button>Meow</Button>
+        <Button>Tick</Button>
+      </Container>
+    </Container>
   );
 };
 
